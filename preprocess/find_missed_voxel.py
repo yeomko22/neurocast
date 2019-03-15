0514161dbf5d6eed19423480ddabc33827ca9084
Hyeong Jun Kim <hjkim2246@gmail.com>
2019-03-15 19:52:48 +0900

    feature/remove-negative-activation-score-data (#9)

--- preprocess/find_missed_voxel.py
+++ preprocess/find_missed_voxel.py
@@ -1,32 +1,6 @@
 import csv
 import os
 
-# Todo: 추후에 복셀 영역별 데이터 셋을 구축해야할 필요성이 생기면 적용할 것 (아마 안할 듯)
-# class VoxelRegion:
-#     def __init__(self, region_id):
-#         self.region_id = region_id
-#         self.score_dict = {}
-#         self.voxel_set = set()
-#
-#     def set_score(self, voxel_id, keyword, score):
-#         self.voxel_set.add(voxel_id)
-#         if not self.score_dict.keys().__contains__(keyword):
-#             self.score_dict.update({keyword: 0.0})
-#         self.score_dict.update({keyword: self.score_dict.get(keyword) + float(score)})
-#
-#     def info(self):
-#         print(self.region_id)
-#         print(self.score_dict)
-#         print(self.voxel_set)
-#
-#
-# def make_sample_coordinates():
-#     coordiates_csv = csv.reader(open(os.path.abspath('../data/coordinates_table.csv'), 'r'))
-#     coordiates_sample = csv.writer(open(os.path.abspath('../data/coordinates_sample.csv'), 'w'))
-#     for i, row in enumerate(coordiates_csv):
-#         if i > 20:
-#             break
-#         coordiates_sample.writerow(row)
 
 def find_missed_voxel():
     voxel_region_dict = {}
@@ -54,6 +28,7 @@ def find_missed_voxel():
     missing_voxel_csv.writerow(['voxel_id'])
     for voxel_id in missing_voxel_list:
         missing_voxel_csv.writerow([voxel_id])
+
 
 if __name__ == '__main__':
     find_missed_voxel()
--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -2,12 +2,14 @@ import csv
 
 import nibabel as nib
 import os
-
+from tqdm import tqdm
+from util import util
 
 # sort output csv file per voxel id (cluster same voxel id)
 # it makes easier to make the data into matrix format
 def sort_data(output_dir, mtype):
-    print('----- %s data sort start -----')
+    logger.info('{0} data sort start'.format(mtype))
+
     raw_data_path = os.path.join(output_dir, mtype + '.csv')
     sorted_data_path = os.path.join(output_dir, mtype + '_sorted.csv')
 
@@ -17,14 +19,14 @@ def sort_data(output_dir, mtype):
 
     data_list = []
     for i, row in enumerate(raw_data):
+        if i==0:
+            continue
         data_list.append(row)
     data_list.sort()
 
     for data in data_list:
         sorted_data.writerow(data)
-    sorted_data.close()
-    raw_data.close()
-    print('----- %s data sort finish -----')
+    logger.info('{0} data sort finish'.format(mtype))
     return sorted_data_path
 
 
@@ -34,7 +36,7 @@ def voxel_check(nib_data, output_csv, keyword):
     for i in range(len(nib_data)):
         for j in range(len(nib_data[0])):
             for k in range(len(nib_data[0][0])):
-                if abs(nib_data[i][j][k]) != 0.0:
+                if nib_data[i][j][k] > 0.0:
                     cur_id = '%d_%d_%d' % (i, j, k)
                     cur_score = nib_data[i][j][k]
                     output_csv.writerow([cur_id, keyword, cur_score])
@@ -43,16 +45,15 @@ def voxel_check(nib_data, output_csv, keyword):
 # read nii file per each keyword
 # request voxel_check per each nii file
 def process(base_dir, mtype, keyword_list, output_csv):
-    print('----- %s data process start -----' % mtype)
+    logger.info('{0} data process start'.format(mtype))
     dir_name = 'keyword_%s_test' % mtype
-    for i, keyword in enumerate(keyword_list):
+
+    for i, keyword in tqdm(enumerate(keyword_list), total=len(keyword_list)):
         file_name = '%s_%s-test_z_FDR_0.01.nii.gz' % (keyword, mtype)
         nib_file = nib.load(os.path.join(base_dir, dir_name, file_name))
         nib_data = nib_file.get_fdata()
         voxel_check(nib_data, output_csv, keyword)
-        print('%s process complete (%d/%d)' % (keyword, i, len(keyword_list)))
-    output_csv.close()
-    print('----- %s data process finish -----' % mtype)
+    logger.info('{0} data process finish'.format(mtype))
 
 
 def main():
@@ -71,15 +72,15 @@ def main():
         keyword_list.append(keyword_line[1])
 
     # process data per measure type
-    measure_types = ['uniformity', 'association']
+    measure_types = ['uniformity']
     for mtype in measure_types:
         output_path = os.path.join(output_dir, mtype + '.csv')
         output_csv = csv.writer(open(output_path, 'w'))
         output_csv.writerow(['voxel_id', 'keyword', 'score'])
         process(base_dir, mtype, keyword_list, output_csv)
         sorted_data_path = sort_data(output_dir, mtype)
-        print('final result: %s' % sorted_data_path)
-
+        logger.info('final result: {0}'.format(sorted_data_path))
 
 if __name__ == '__main__':
+    logger = util.get_logger()
     main()
